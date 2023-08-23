bcab0f39150dd86fc550d4d27498bb0f4329ce7e
miruiliescu <iliescumiruna@yahoo.com>
2023-08-23 19:30:36 +0300

    function for one round

--- index.js
+++ index.js
@@ -6,4 +6,46 @@ function getComputerChoice(){
     let i = Math.floor(Math.random() * 3);
     return n[i];
 }
-console.log(getComputerChoice());  
+
+ 
+
+function playRound() {
+
+    let computerSelection = getComputerChoice();
+    let playerSelection = prompt("your choice: ").toLowerCase();
+    
+    console.log(`computer's choice: ${computerSelection}`);
+   
+ 
+    if (playerSelection == computerSelection){
+        console.log( "equality!")
+    }
+
+    if (playerSelection == "rock" && computerSelection == "paper"){
+        console.log("You lose! Paper beats rock :(")
+    }
+
+    if (playerSelection == "rock" && computerSelection == "scissor"){
+        console.log("You win! Rock beats scissor :)")
+    }
+
+    if (playerSelection == "paper" && computerSelection == "rock"){
+        console.log("You win! Paper beats rock :) ")
+    }
+
+    if (playerSelection == "paper" && computerSelection == "scissor"){
+        console.log("You lose! Scissor beats paper :(")
+    }
+
+    if (playerSelection == "scissor" && computerSelection == "rock"){
+        console.log( " You lose! Rock beats scissor :(")
+    }
+
+    if (playerSelection == "scissor" && computerSelection == "paper"){
+        console.log( "You win! Scissor beats paper :)")
+    }
+    
+}
+
+
+playRound();
