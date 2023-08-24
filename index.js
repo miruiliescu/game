ccf2f60e3f0ebf67ac2e18d227012cf8038e1ec9
miruiliescu <iliescumiruna@yahoo.com>
2023-08-24 19:16:38 +0300

    function "game" holds score and declares winner

--- index.js
+++ index.js
@@ -19,33 +19,78 @@ function playRound() {
  
     if (playerSelection == computerSelection){
         console.log( "equality!")
+        return 0;
     }
 
     if (playerSelection == "rock" && computerSelection == "paper"){
         console.log("You lose! Paper beats rock :(")
+        return 1;
     }
 
     if (playerSelection == "rock" && computerSelection == "scissor"){
         console.log("You win! Rock beats scissor :)")
+        return 2;
     }
 
     if (playerSelection == "paper" && computerSelection == "rock"){
         console.log("You win! Paper beats rock :) ")
+        return 2;
     }
 
     if (playerSelection == "paper" && computerSelection == "scissor"){
         console.log("You lose! Scissor beats paper :(")
+        return 1;
     }
 
     if (playerSelection == "scissor" && computerSelection == "rock"){
         console.log( " You lose! Rock beats scissor :(")
+        return 1;
     }
 
     if (playerSelection == "scissor" && computerSelection == "paper"){
         console.log( "You win! Scissor beats paper :)")
+        return 2;
     }
     
 }
 
 
-playRound();
+
+
+function game () {
+    let playerScore = 0;
+    let computerScore = 0;
+    let score = 0;
+    let numberOfRounds = 0;
+    do {
+        score = playRound();
+       
+        if(score== 1) {
+            computerScore ++;
+            
+
+        } 
+        
+        
+        if (score == 2) {
+            playerScore ++;
+        }
+
+        numberOfRounds ++;
+        
+           
+        
+    } while (numberOfRounds < 5)
+    
+    if (playerScore > computerScore) {
+        console.log ("YOU WIN!!");
+    }
+   else if (computerScore > playerScore) {
+        console.log ("YOU LOSE!")
+    }
+    if (playerScore == computerScore) {
+        console.log ("TIE!");
+    }
+}
+
+game();
